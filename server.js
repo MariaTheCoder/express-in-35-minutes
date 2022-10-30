5ec574a5c468648a656eee7dd79ea2679b249b1f
Maria T. Andersen <mteilmann93@gmail.com>
2022-10-30 22:30:19 +0100

    In many client-serverside-communication use cases, 'message' might not be passed down to the server. To avoid an error stating that message is undefined, we can use locals.message. Locals will always be defined, and if it has no property called message, it will return 'Default'.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   console.log("Hello World");
-  res.render("index", { message: "Maria" });
+  res.render("index", { message123: "Maria" });
 });
 
 app.listen(3000);
