b8bfb19978b8d18d495d64368fe5934326644313
Maria T. Andersen <mteilmann93@gmail.com>
2022-10-30 22:14:45 +0100

    Set the view engine to ejs and use boilerplate code in the ejs file. Test if the code in this file is being rendered correctly.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   console.log("Hello World");
-  res.send("Hi");
+  res.render("index");
 });
 
 app.listen(3000);
