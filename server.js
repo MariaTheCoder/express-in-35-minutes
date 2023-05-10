4e63a7cfea03f78ddb4ae989bd3e95fd342afb1b
Maria T. Andersen <mteilmann93@gmail.com>
2023-05-10 15:40:55 +0200

    use the port specified in the enviroment variable PORT and if it is not specified, use port 3050. Create mock data and test the server connection the a http get request.

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
+const port = process.env.port || 3050;
 
-app.set("view engine", "ejs");
+// data
+const users = [
+  { id: 0, name: "Maria", email: "example1@email.com" },
+  { id: 1, name: "Sanne", email: "example2@email.com" },
+  { id: 2, name: "Nina", email: "example3@email.com" },
+];
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  console.log("Hello World");
-  res.render("index", { message123: "Maria" });
+  res.send(users);
 });
 
-app.listen(3000);
+app.listen(port, (err) => {
+  if (err) console.log(err);
+  console.log(`Listening on port ${port}`);
+});
